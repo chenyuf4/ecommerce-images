f66f780c17c4f2065a4fac36f65ac0759e0231d0
chenyuf2 <42945468+chenyuf2@users.noreply.github.com>
2022-07-23 01:50:41 -0700

    clean some code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,36 @@
 import styles from "./Home.module.scss";
 import Icon from "components/Icon/Icon";
+import clsx from "clsx";
 const Home = () => {
   return (
     <div className={styles["home-container"]}>
-      <div className="p-5">
-        <div className={styles["logo-container"]}>
+      <div className="p-5 d-flex justify-content-between align-items-center">
+        <div
+          className={clsx(
+            styles["logo-container"],
+            "d-flex justify-content-center align-items-center"
+          )}
+        >
           <Icon />
+        </div>
+        <div className="d-flex">
+          <div
+            className={clsx(
+              styles["mode-logo-container"],
+              "d-flex justify-content-center align-items-center"
+            )}
+          >
+            <Icon iconType="slide" />
+          </div>
+          <div
+            className={clsx(
+              styles["mode-logo-container"],
+              styles["mode-inactive"],
+              "ms-3 d-flex justify-content-center align-items-center"
+            )}
+          >
+            <Icon iconType="grid" />
+          </div>
         </div>
       </div>
     </div>
